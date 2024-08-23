11b1769ce9af9e2416b297633f690b2931d3ddd1
unknown <noylyguzmanalva@gmail.com>
2024-08-23 17:27:46 -0500

    primer ejercicio

---
+++ incrustado-01.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My primer programa en PHP</title>
+</head>
+<body>
+<h1><?php echo "hola mundo desde PHP";?></h1>
+</body>
+</html>
